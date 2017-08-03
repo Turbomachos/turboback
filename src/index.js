003a3cf5529236937bc2c8c67e4d54569650da7d
Francisco Mus Chovares <al150652@uji.es>
2017-08-03 20:48:34 +0200

    cambio datos

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
 import config from './config.json';
-
 let app = express();
 app.server = http.createServer(app);
 
