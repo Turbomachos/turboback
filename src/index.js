82331c5acd28b3dc9560dd16fad050769ef2b3fc
Francisco Mus Chovares <al150652@uji.es>
2017-10-12 12:00:03 +0200

    swagger comment strue

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ var swaggerDefinition = {
         description: 'Demonstrating how to describe a RESTful API with Swagger',
     },
     schemes: ["http", "https"],
-    host: 'https://www.turbomachos.com',
+    host: 'http://www.turbomachos.com',
     basePath: '/back/v1',
 };
 var options = {
