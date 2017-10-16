7e10194386f11a06c252d9b8cd0fc3df3bdc9aa5
Francisco Mus Chovares <al150652@uji.es>
2017-10-16 22:49:39 +0200

    xxxx

--- src/api/index.js
+++ src/api/index.js
@@ -53,10 +53,10 @@ export default ({ config, db }) => {
     });
 
     api.get('/usuarios', (req, res) => {
-        connection.query('INSERT INTO usuarios VALUES(0, "pakatanga", "tortilla", "pmus@turbomachos.com", "", "")', undefined, (cosa) =>{
+        connection.query('INSERT INTO usuarios VALUES(0, "pakatanga", "tortilla", "pmus@turbomachos.com", "", "")',(cosa) =>{
             console.log(cosa);
         });
-        connection.query('select * from usuarios', undefined, (result)=>{
+        connection.query('select * from usuarios', (result)=>{
             return result;
         });
     });
