16cc889f8ee4305d4c3ac1365b01a0705b9498ad
Francisco Mus Chovares <al150652@uji.es>
2017-10-28 00:26:16 +0200

    handle error mysql connection

--- src/mysql/index.js
+++ src/mysql/index.js
@@ -1,20 +1,36 @@
 
 import mysql from 'mysql';
 
-var connection = mysql.createConnection({
+var mysql_config = {
     host:'www.turbomachos.com',
     user:'turbouser',
     password : 'Turbo.machos91',
     database: 'turbomachos',
     port: '3306'
-});
+};
 
-connection.connect(function(error){
-    if(error){
-        console.log('error conectando a mysql: ' + error.stack);
-    }else{
-        console.log('conexión de maravilla');
-    }
-});
+var connection;
+
+var handleDisconnect = () => {
+    connection = mysql.createConnection(mysql_config);
+    connection.connect(function(error){
+        if(error){
+            console.log('error conectando a mysql: ' + error.stack);
+        }else{
+            console.log('conexión de maravilla');
+        }
+    });
+
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+};
+
+handleDisconnect();
 
 export default connection;
